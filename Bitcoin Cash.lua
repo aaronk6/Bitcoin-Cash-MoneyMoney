51d1df82b1d75b5cd52687fea3f17c50462c3f20
aaronk6 <aaronk6@users.noreply.github.com>
2020-03-13 16:41:27 +0100

    Use bitcoin.com to retrieve balance, support multiple addresses, warn when addresses don’t start with bitcoincash:

--- Bitcoin Cash.lua
+++ Bitcoin Cash.lua
@@ -1,14 +1,12 @@
--- Inofficial Bitcoin Extension for MoneyMoney
--- Fetches Ether quantity for address via etherscan API
--- Fetches Ether price in EUR via cryptocompare API
--- Returns cryptoassets as securities
+-- Inofficial Bitcoin Cash Extension for MoneyMoney
+-- Fetches balances from bitcoin.com and returns them as securities
 --
--- Username: Bitcoin Adresses comma seperated
--- Password: [Whatever]
+-- Username: Bitcoin Cash adresses (need to start with "bitcoincash:", comma seperated)
+-- Password: (anything)
 
 -- MIT License
 
--- Copyright (c) 2020 Jacubeit, aaronk6
+-- Copyright (c) 2020 aaronk6
 
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
@@ -28,29 +26,46 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 
-
 WebBanking{
   version = 1.00,
   description = "Include your Bitcoin Cash as cryptoportfolio in MoneyMoney by providing Bitcoin Cash addresses as usernme (comma seperated) and a random Password",
   services= { "Bitcoin Cash" }
 }
 
-local bitcoinAddress
-local connection = Connection()
-local currency = "EUR" -- fixme: make dynamik if MM enables input field
+local currencyName = "Bitcoin Cash"
+local currency = "EUR" -- fixme: Don't hardcode
+local currencyField = "eur"
+local currencyId = "bitcoin-cash"
+local currencyDecimals = 8
+local marketName = "CoinGecko"
+local priceUrl = "https://api.coingecko.com/api/v3/simple/price?ids=" .. currencyId .. "&vs_currencies=" .. currencyField
+local balanceUrl = "https://rest.bitcoin.com/v2/address/details/"
+
+local addresses
+local balances
 
 function SupportsBank (protocol, bankCode)
   return protocol == ProtocolWebBanking and bankCode == "Bitcoin Cash"
 end
 
 function InitializeSession (protocol, bankCode, username, username2, password, username3)
-  bitcoinAddress = username:gsub("%s+", "")
+  addresses = strsplit(",%s*", username)
+  required_prefix = "bitcoincash:"
+
+  for i,v in ipairs(addresses) do
+    if v:sub(1, #required_prefix) ~= required_prefix then
+      custom_error(
+        "Unsupported address “" .. v .. "”",
+        "Note that all addresses must start with “" .. required_prefix .. "”. " ..
+        "If your address is in legacy format, you can convert it here:\n\n" .. "https://cashaddr.bitcoincash.org/")
+    end
+  end
 end
 
 function ListAccounts (knownAccounts)
   local account = {
-    name = "Bitcoin Cash",
-    accountNumber = "Bitcoin Cash",
+    name = currencyName,
+    accountNumber = currencyName,
     currency = currency,
     portfolio = true,
     type = "AccountTypePortfolio"
@@ -61,17 +76,16 @@ end
 
 function RefreshAccount (account, since)
   local s = {}
-  prices = requestBitcoinPrice()
+  local prices = queryPrices()
+  local balances = queryBalances(addresses)
 
-  for address in string.gmatch(bitcoinAddress, '([^,]+)') do
-    bitcoinQuantity = requestBitcoinQuantityForBitcoinAddress(address)
-
-    s[#s+1] = {
-      name = address,
+  for i,v in ipairs(addresses) do
+    s[i] = {
+      name = currencyName .. " · " .. v,
       currency = nil,
-      market = "cryptocompare",
-      quantity = bitcoinQuantity,
-      price = prices["price_eur"],
+      market = marketName,
+      quantity = balances[i] / math.pow(10, currencyDecimals),
+      price = prices[currencyField],
     }
   end
 
@@ -81,33 +95,45 @@ end
 function EndSession ()
 end
 
-
--- Querry Functions
-function requestBitcoinPrice()
-  response = connection:request("GET", cryptocompareRequestUrl(), {})
-  json = JSON(response)
-
-  return json:dictionary()[1]
+function queryPrices()
+  local connection = Connection()
+  local res = JSON(connection:request("GET", priceUrl))
+  return res:dictionary()[currencyId]
 end
 
-function requestBitcoinQuantityForBitcoinAddress(bitcoinAddress)
-  response = connection:request("GET", bitcoinRequestUrl(bitcoinAddress), {})
-  json = JSON(response)
-  
-  return convertSatoshiToBitcoin(response)
+function queryBalances(addresses)
+  local connection = Connection()
+  local balances = {}
+  local res
+
+  for key, address in pairs(addresses) do
+    res = JSON(connection:request("GET", balanceUrl .. address))
+    table.insert(balances, res:dictionary()["balanceSat"])
+  end
+
+  return balances
 end
 
-
--- Helper Functions
-function convertSatoshiToBitcoin(satoshi)
-  return satoshi / 100000000  
+-- from http://lua-users.org/wiki/SplitJoin
+function strsplit(delimiter, text)
+  local list = {}
+  local pos = 1
+  if string.find("", delimiter, 1) then -- this would result in endless loops
+    error("delimiter matches empty string!")
+  end
+  while 1 do
+    local first, last = string.find(text, delimiter, pos)
+    if first then -- found?
+      table.insert(list, string.sub(text, pos, first-1))
+      pos = last+1
+    else
+      table.insert(list, string.sub(text, pos))
+      break
+    end
+  end
+  return list
 end
 
-function cryptocompareRequestUrl()
-  return "https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/?convert=EUR"
-end 
-
-function bitcoinRequestUrl(bitcoinAddress)
-  return "https://bitcoincash.blockexplorer.com/api/addr/" .. bitcoinAddress .. "/balance"
+function custom_error(title, msg)
+  error("\n\n⚠️ " .. title .. "\n\n" .. msg)
 end
-
